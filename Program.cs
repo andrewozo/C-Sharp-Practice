bb31fd692768f6002f91b247fd14956615d22891
Andrew <andrewozo123@gmail.com>
2023-12-08 11:03:06 -0500

    choosing correct data type exercise

--- Program.cs
+++ Program.cs
@@ -1,90 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// string? readResult;
-// bool validEntry = false;
-// int numValue = 0;
+// Console.WriteLine("Signed integral types");
 
-// Console.WriteLine("Enter a number between 5 and 10");
+// Console.WriteLine($"sbyte : {sbyte.MinValue} to {sbyte.MaxValue}");
+// Console.WriteLine($"short : {short.MinValue} to {short.MaxValue} ");
+// Console.WriteLine($"int : {int.MinValue} to {int.MaxValue}");
+// Console.WriteLine($"long : {long.MinValue} to {long.MaxValue}");
 
-// do
-// {
-//     readResult = Console.ReadLine();
-//     bool isNum = int.TryParse(readResult, out numValue);
-//     if (readResult != null)
-//     {
-//         if (isNum)
-//         {
-//             if ((numValue >= 5) && (numValue <= 10))
-//             {
-//                 validEntry = true;
-//             }
-//             else
-//             {
-//                 Console.WriteLine($"You Entered {numValue} please enter a number between 5 and 10");
-//             }
-//         }
-//         else
-//         {
-//             Console.WriteLine("Sorry you entered an invalid number please try again");
-//         }
-//     }
-// } while (validEntry == false);
+// Console.WriteLine("");
+// Console.WriteLine("Unsigned integral types");
 
-// Console.WriteLine($"Your input ({numValue}) has been accepted");
+// Console.WriteLine($"byte : {byte.MinValue} to {byte.MaxValue}");
+// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+// Console.WriteLine($"uint : {uint.MinValue} to {uint.MaxValue}");
+// Console.WriteLine($"ulong : {ulong.MinValue} to {ulong.MaxValue}");
+
+// Console.WriteLine("");
+// Console.WriteLine("Floating point types");
+
+// Console.WriteLine($"float : {float.MinValue} to {float.MaxValue} (with 6-9 digits of precision)");
+// Console.WriteLine(
+//     $"double : {double.MinValue} to {double.MaxValue} (with 15-17 digits of precision) "
+// );
+// Console.WriteLine(
+//     $"decimal : {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)"
+// );
 
 
-// string? readResult;
-// bool validEntry = false;
-
-// Console.WriteLine("Enter your role name (administrator, manager, or user)");
-
-// while (validEntry == false)
-// {
-//     readResult = Console.ReadLine();
-
-//     if (readResult != null)
-//     {
-//         if ((readResult == "administrator") || (readResult == "manager") || (readResult == "user"))
-//         {
-//             validEntry = true;
-//             Console.WriteLine($"Your input value ({readResult}) has been accepted");
-//         }
-//         else
-//         {
-//             Console.WriteLine(
-//                 $"The role name that you entered, '{readResult}' is not valid. Enter your role name (administrator, manager, or user)"
-//             );
-//         }
-//     }
-// }
-
-string[] myStrings = new string[2]
-{
-    "I like pizza. I like roast chicken. I like salad",
-    "I like all three of the menu choices"
-};
-string myString = "";
-int periodLocation = 0;
-
-for (int i = 0; i < myStrings.Length; i++)
-{
-    myString = myStrings[i];
-    periodLocation = myString.IndexOf(".");
-    string mySentence;
-
-    while (periodLocation != -1)
-    {
-        mySentence = myString.Remove(periodLocation);
-
-        myString = myString.Substring(periodLocation + 1);
-
-        myString = myString.TrimStart();
-
-        periodLocation = myString.IndexOf(".");
-
-        Console.WriteLine(mySentence);
-    }
-
-    mySentence = myString.Trim();
-    Console.WriteLine(mySentence + "!");
-}
+int[] data = new int[3];
+string shortenedString = "Hello World";
+Console.WriteLine(shortenedString);
