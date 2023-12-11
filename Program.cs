fceb301fdedea25924e3d2a9e99fd832e95cd8f1
Andrew <andrewozo123@gmail.com>
2023-12-11 14:50:39 -0500

    perform operations on arrays using helper methods in c#

--- Program.cs
+++ Program.cs
@@ -1,98 +1,106 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// int first = 2;
-// string second = "4";
-// string result = first + second;
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
+
+
+
+// string[] pallets = { "B14", "A11", "B12", "A13" };
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"--{pallet}");
+// }
+
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
 // Console.WriteLine(result);
 
-// int myInt = 3;
-// Console.WriteLine($"int: {myInt}");
+// string[] items = result.Split(",");
+// foreach (var item in items)
+// {
+//     Console.WriteLine(item);
+// }
 
-// decimal myDecimal = myInt;
-// Console.WriteLine($"decimal: {myDecimal}");
+// string pangram = "The quick brown fox jumps over the lazy dog";
+// string result = "";
 
-// decimal myDecimal = 3.14m;
-// Console.WriteLine($"decimal: {myDecimal}");
+// string[] words = pangram.Split(" ");
+// string[] reversedWords = new string[words.Length];
 
-// int myInt = (int)myDecimal;
-// Console.WriteLine($"int: {myInt}");
+// for (int i = 0; i < words.Length; i++)
+// {
+//     char[] wordArr = words[i].ToCharArray();
+//     Array.Reverse(wordArr);
+//     string reversedWord = new string(wordArr);
 
-// decimal myDecimal = 1.23456789m;
-// float myFloat = (float)myDecimal;
+//     reversedWords[i] = reversedWord;
+// }
 
-// Console.WriteLine($"Decimal: {myDecimal}");
-// Console.WriteLine($"Float: {myFloat}");
+// result = String.Join(" ", reversedWords);
 
-// int first = 5;
-// int second = 7;
-// string message = first.ToString() + second.ToString();
-// Console.WriteLine(message);
-
-// string first = "5";
-// string second = "7";
-
-// int sum = int.Parse(first) + int.Parse(second);
-
-// Console.WriteLine(sum);
-
-
-// string value1 = "5";
-// string value2 = "7";
-
-// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
 // Console.WriteLine(result);
 
 
-// int value = (int)1.5m;
-// Console.WriteLine(value);
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
 
-// int value2 = Convert.ToInt32(1.5m);
-// Console.WriteLine(value2);
+string[] orders = orderStream.Split(",");
+Array.Sort(orders);
 
-// string value = "bad";
-// int result = 0;
-
-// if (int.TryParse(value, out result))
-// {
-//     Console.WriteLine($"Measurment: {result}");
-// }
-// else
-// {
-//     Console.WriteLine("Unable to report the measurement");
-// }
-
-// if (result > 0)
-// {
-//     Console.WriteLine($"Measurement (w/offset): {50 + result}");
-// }
-
-
-// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-// string message = "";
-// decimal total = 0;
-// decimal currentValue = 0;
-
-// for (int i = 0; i < values.Length;i++){
-
-//     if (decimal.TryParse(values[i], out currentValue)){
-//         total += currentValue;
-//     } else {
-//         message += values[i];
-//     }
-// }
-
-// Console.WriteLine($"Message: {message}");
-// Console.WriteLine($"Total: {total}");
-
-int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
-
-int result1 = Convert.ToInt32(value1) / Convert.ToInt32(value2);
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
-
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+foreach (var order in orders)
+{
+    if (order.Length == 4)
+    {
+        Console.WriteLine(order);
+    }
+    else
+    {
+        Console.WriteLine(order + "\t- Error");
+    }
+}
